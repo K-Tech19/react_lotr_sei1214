fbb199348366005054131f4c68232e6b7cbb2838
nickubed <nhpquandt@gmail.com>
2021-02-01 14:11:34 -0800

    cool beans

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
 
 function App() {
+  let newStr = 'String Stuff!'
+  let movieArr = [
+    {
+      title: "The Fellowship of the Ring",
+      hours: "2",
+      minutes: "58"
+    },
+    {
+      title: "The Two Towers",
+      hours: "2",
+      minutes: "59"
+    },
+    {
+      title: "The Return of the Cat",
+      hours: "50",
+      minutes: "A billion"
+    },
+    {
+      title: 'More Cats',
+      hours: '200',
+      minutes: '5'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{newStr}</h1>
+      <h1>{movieArr.map((movie, i) => {
+        return <Movie title={movie.title} hours={movie.hours} minutes={movie.minutes} />
+      })}</h1>
     </div>
   );
 }
